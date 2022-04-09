a4c047cc04d4922f83835b6dd82c4794e175ab9f
Adelya <valisheva2002@mail.ruclear>
2022-04-09 15:02:31 +0300

    base ui created

---
+++ app/src/main/java/kpfu/itis/valisheva/knb_game/CardGameFragmentFirst.kt
@@ -0,0 +1,6 @@
+package kpfu.itis.valisheva.knb_game
+
+import androidx.fragment.app.Fragment
+
+class CardGameFragmentFirst: Fragment(R.layout.fragment_card_game_first) {
+}
---
+++ app/src/main/java/kpfu/itis/valisheva/knb_game/PlayerChallengeFragment.kt
@@ -0,0 +1,6 @@
+package kpfu.itis.valisheva.knb_game
+
+import androidx.fragment.app.Fragment
+
+class PlayerChallengeFragment: Fragment(R.layout.fragment_player_challenge) {
+}
